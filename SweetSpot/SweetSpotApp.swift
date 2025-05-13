d4d8c04f5932f162ebdb8d5db95c1fbb54d64442
Dylan Kereluk <dylan.kereluk@gmail.com>
2025-05-13 11:29:10 -0700

    Initial Commit

---
+++ SweetSpot/SweetSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSpotApp.swift
+//  SweetSpot
+//
+//  Created by Dylan Kereluk on 2025-05-13.
+//
+
+import SwiftUI
+
+@main
+struct SweetSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
